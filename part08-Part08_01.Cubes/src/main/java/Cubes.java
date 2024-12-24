ea537c38465e16c9c51ef2ab6219bb143f7b0e65
AlexKano10 <alex40806@gmail.com>
2024-12-24 10:31:38 +0100

    exercise done part08_Part08_01.Cubes

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -6,5 +6,17 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-    }
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("end")){
+                break;
+            } try {
+                int number = Integer.valueOf(input);
+                System.out.println(number * number * number);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter a number.");
+                // TODO: handle exception
+            }
+        }
+    }   
 }
