2b20c6e266fd2f1d58d860386b510638b49dcd2f
AlexKano10 <alex40806@gmail.com>
2025-01-27 08:36:02 +0100

    done exercice part08_10.IOweYou

--- part08-Part08_10.IOweYou/src/main/java/Main.java
+++ part08-Part08_10.IOweYou/src/main/java/Main.java
@@ -3,5 +3,22 @@ public class Main {
 
     public static void main(String[] args) {
         // Test your program here
+
+        IOU mattsIOU = new IOU();
+
+        // Registrar deudas
+        mattsIOU.setSum("Arthur", 51.5);
+        mattsIOU.setSum("Michael", 30);
+
+        // Consultar deudas
+        System.out.println(mattsIOU.howMuchDoIOweTo("Arthur"));  // Salida: 51.5
+        System.out.println(mattsIOU.howMuchDoIOweTo("Michael")); // Salida: 30.0
+        System.out.println(mattsIOU.howMuchDoIOweTo("John"));    // Salida: 0.0
+
+        // Actualizar deuda
+        mattsIOU.setSum("Arthur", 10.5);
+        System.out.println(mattsIOU.howMuchDoIOweTo("Arthur"));  // Salida: 10.5
+    
+
     }
 }
