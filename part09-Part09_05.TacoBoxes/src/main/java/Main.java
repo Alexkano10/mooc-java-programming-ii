f14fe12381c1f273be4f41767f388f2c0f1fb1c2
AlexKano10 <alex40806@gmail.com>
2025-02-10 19:17:33 +0100

    exercise part09_05.TacoBoxes

--- part09-Part09_05.TacoBoxes/src/main/java/Main.java
+++ part09-Part09_05.TacoBoxes/src/main/java/Main.java
@@ -3,5 +3,17 @@ public class Main {
 
     public static void main(String[] args) {
         // Test your code here!
+
+        CustomTacoBox customBox = new CustomTacoBox(5); // Caja con 5 tacos
+        System.out.println(customBox.tacosRemaining()); // 5
+        customBox.eat();
+        System.out.println(customBox.tacosRemaining()); // 4
+        customBox.eat();
+        customBox.eat();
+        customBox.eat();
+        customBox.eat();
+        customBox.eat(); // Intento comer más de lo que hay
+        System.out.println(customBox.tacosRemaining()); // 0
+
     }
 }
